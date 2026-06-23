62297c76e693f297c9db74e25a078689dc7ffe9e
Ankit Patel <ankitdilip@gmail.com>
2026-06-23 10:34:59 +0530

    Enable SoftDeletes for academic records

--- app/Models/QuizAnswer.php
+++ app/Models/QuizAnswer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class QuizAnswer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'quiz_attempt_id',
